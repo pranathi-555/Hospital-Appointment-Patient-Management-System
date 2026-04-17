487792cada3ed36780273afc06cbdaf1af099370
roshini018 <b23cs107@kitsw.ac.in>
2026-04-18 01:13:15 +0530

    Fix

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,38 +1,8 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:5001",
-//   withCredentials: true,
-// });
-
-// // Request interceptor - attach token from localStorage if available
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
-// // Response interceptor – silently handle 401 without crashing
-// api.interceptors.response.use(
-//   (res) => res,
-//   (err) => {
-//     // Don't redirect on 401 from /me – AuthContext handles it
-//     return Promise.reject(err);
-//   }
-// );
-
-// export default api;
-
 
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001",
+  baseURL: "https://hospitalpatientmanagement-1.onrender.com",
   withCredentials: true,
 });
 
